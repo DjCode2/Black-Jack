21a79e88cc236f02bf18eb6eab7a57e8a02795a0
DjCode2 <99595748+DjCode2@users.noreply.github.com>
2024-04-05 17:16:10 +0200

    Update fonction.py
    ajout du menu plus de tapis de jeu plus du score des fonction quitter abandonner enregistrer ect

--- fonction.py
+++ fonction.py
@@ -1,14 +1,32 @@
-#fonction.py
 import tkinter as tk
+import fonction
 
-def quitter_menu(root_menu):
-    root_menu.quit()
+# Crée le menu
+def creer_menu():
+    root_menu = tk.Tk()
+    root_menu.title("Menu")
 
-def ouvrir_fenetre_jeu(root_menu):
-    # Fermer la fenêtre de menu
-    root_menu.destroy()
-    # Ouvrir la fenêtre de jeu principal
-    root_jeu = tk.Tk()
-    root_jeu.title("BlackJack")
-    # Ajoutez ici le contenu de votre fenêtre de jeu principal
-    root_jeu.mainloop()
+    largeur_fenetre_menu = 1230
+    hauteur_fenetre_menu = 762
+    root_menu.resizable(False, False)
+
+    # Charger l'image de fond
+    photo = tk.PhotoImage(file="BlackjackMenu.png")
+    image_label = tk.Label(root_menu, image=photo)
+    image_label.pack()
+
+    # Définir la taille de la fenêtre indépendamment du contenu
+
+    # Définir les boutons du menu
+    jouer_button = tk.Button(root_menu, text="Jouer", font=("Arial", 25), width=10, command=lambda: fonction.ouvrir_fenetre_jeu(root_menu))
+    quitter_button = tk.Button(root_menu, text="Quitter", width=10, font=("Arial", 25), bg="red", command=root_menu.quit)
+
+    # Placement du titre et des boutons du menu
+    jouer_button.place(x=520, y=450)
+    quitter_button.place(x=520, y=520)
+
+    # Lancer la boucle principale
+    root_menu.mainloop()
+
+
+creer_menu()
