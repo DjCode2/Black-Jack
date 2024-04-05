f6b3207a593a990698f3cf44851842e1e6c27050
DjCode2 <99595748+DjCode2@users.noreply.github.com>
2024-04-05 17:18:53 +0200

    Update mainloop.py
    ajout des fonctions quitter sauvegarder et abandonné ainsi que du score

--- mainloop.py
+++ mainloop.py
@@ -1,41 +1,35 @@
-# main.py
+import tkinter as tk
+import fonction
 
-while True:
-    try:
-        import os
-        import tkinter as tk
-        import fonction
-        print("l'import c'est bien passé")
-        break #on sort si les instal se sont bien passé
-    except ImportError:
-        print("erreur d'import, une des bibliothèques n'est pas sur la machine ou n'est pas reconnue")
-        break
+# Crée le menu
+def creer_menu():
+    root_menu = tk.Tk()
+    root_menu.title("Menu")
 
-# crée le menu
-root_menu = tk.Tk()
-root_menu.title("Menu")
+    largeur_fenetre_menu = 1230
+    hauteur_fenetre_menu = 762
+    root_menu.resizable(False, False)
 
-largeur_fenetre_menu = 1230
-hauteur_fenetre_menu = 762
-root_menu.resizable(False, False)
-#gérer le fond
-photo = tk.PhotoImage(file="BlackjackMenu.png")
-image_label = tk.Label(root_menu, image=photo)
-image_label.pack()
-root_menu.geometry(f"{largeur_fenetre_menu}x{hauteur_fenetre_menu}") #définir la taille de la fenetre indépendament du contenu
+    # Charger l'image de fond
+    photo = tk.PhotoImage(file="BlackjackMenu.png")
+    image_label = tk.Label(root_menu, image=photo)
+    image_label.pack()
 
-#def les boutons du menu
-jouer_button = tk.Button(root_menu, text="Jouer",font=("Arial", 25), width=10, command=lambda: fonction.ouvrir_fenetre_jeu(root_menu))
-quitter_button = tk.Button(root_menu, text="Quitter", width=10, font=("Arial", 25), bg="red" , command=lambda: fonction.quitter_menu(root_menu))
+    # Définir la taille de la fenêtre indépendamment du contenu
 
-#placer le titre et les boutons du menu
-jouer_button.place(x=520,y=450)
-quitter_button.place(x=520, y=520)
+    # Définir les boutons du menu
+    jouer_button = tk.Button(root_menu, text="Jouer", font=("Arial", 25), width=10, command=lambda: fonction.ouvrir_fenetre_jeu(root_menu))
+    quitter_button = tk.Button(root_menu, text="Quitter", width=10, font=("Arial", 25), bg="red", command=root_menu.quit)
+
+    # Placement du titre et des boutons du menu
+    jouer_button.place(x=520, y=450)
+    quitter_button.place(x=520, y=520)
+
+    # Lancer la boucle principale
+    root_menu.mainloop()
 
 
-
-
-
+creer_menu()
 
 
 
