e249f767d31a61b13a4e5aa0e97b1ab3001af88a
DjCode2 <99595748+DjCode2@users.noreply.github.com>
2024-04-08 14:53:48 +0200

    Update fonction.py
    (add du tapis trool)

--- fonction.py
+++ fonction.py
@@ -3,19 +3,25 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 import datetime
+import FctBlackJack
 import json
 
 
 banque_joueur=0
 score=0
+mainJ = ""
 
 def quitter_menu(root_menu):
     root_menu.quit()
 
 
-def changeLabel(nouvelvaleur,label):
-    label.config(text=f"Score: {nouvelvaleur}")
+def ajouter_au_score(nouvelle_valeur, label):
+    # Récupérer la valeur actuelle du score depuis le label
+    score_actuel = int(label.cget("text").split(":")[1].strip())
     
+    nouveau_score = score_actuel + nouvelle_valeur
+    label.config(text=f"Score : {nouveau_score}")
+
 
 def sauvegarder(score):
     # chopper la date 
@@ -38,8 +44,11 @@ def quitter(root_jeu):
 def demander_carte():#remplace action_joueur
     pass
     
-def doubler_mise():#mise variable global? + ajouter variable banque_joueur qui sera la totalité des jetons disponibles 
+def doubler_mise(score_label):#mise variable global? + ajouter variable banque_joueur qui sera la totalité des jetons disponibles 
+    
     pass
+
+
 def garder_main():
     pass
 
@@ -47,9 +56,10 @@ def abandonner(root_jeu):
     if messagebox.askokcancel("oh la ptite bite", "Êtes-vous sûr de vouloir abandonner ?"):
         root_jeu.destroy()
 
+def affiche_mainJ(mainj,label):
+    label.config(text=f"Score : {mainj}")
 
-
-def ouvrir_fenetre_jeu(root_menu):
+def BouclePrincipale(root_menu):
     # Fermer la fenêtre de menu
     
     root_menu.destroy()
@@ -59,7 +69,7 @@ def ouvrir_fenetre_jeu(root_menu):
     hauteur_fenetre_jeu = 810
 
     # Charger l'image du tapis de blackjack
-    image_tapis = tk.PhotoImage(file="BlackJack.png")
+    image_tapis = tk.PhotoImage(file="tapis_trool.png")
 
     # Créer un canvas pour afficher l'image du tapis de blackjack
     canvas = tk.Canvas(root_jeu, width=largeur_fenetre_jeu, height=hauteur_fenetre_jeu)
@@ -83,6 +93,9 @@ def ouvrir_fenetre_jeu(root_menu):
     score_label = tk.Label(root_jeu, text=f"Score: {score}", font=("Arial", 16))
     score_label.place(x=20, y=20)  # Ajuster la position selon vos besoins
 
+    mainJ_label = tk.Label(root_jeu, text=f"main joueur: {mainJ}", font=("Arial", 16))
+    mainJ_label.place(x=500, y=700)  # Ajuster la position selon vos besoins
+
     btn_save = tk.Button(root_jeu, text="Sauvegarder", width=13, font=("Arial", 15), command=lambda: sauvegarder(score))
     btn_save.place(x=1282, y=7)
 
@@ -95,10 +108,44 @@ def ouvrir_fenetre_jeu(root_menu):
     btn_demande_carte = tk.Button(root_jeu, text="Demander carte(s)", width=15, font=("Arial", 14), command=demander_carte)
     btn_demande_carte.place(x=470, y=750)
 
-    btn_double_mise = tk.Button(root_jeu, text="Doubler la mise", width=15, font=("Arial", 14), command=doubler_mise)
+    btn_double_mise = tk.Button(root_jeu, text="Doubler la mise", width=15, font=("Arial", 14), command=lambda:ajouter_au_score(2,score_label))
     btn_double_mise.place(x=650, y=750)
 
     btn_garder_main = tk.Button(root_jeu, text="Garder la main", width=15, font=("Arial", 14), command=garder_main)
     btn_garder_main.place(x=830, y=750)
+
+    #relatif au jeu --------------------------------------------------
+
+    # programme principale / sera plus tard dans un autre dossier
+    jeu=FctBlackJack.jeu_de_carte()
+    joueur=FctBlackJack.debut()
+
+    main_du_joueur=[]
+    main_du_croupier=[]
+
+    score_du_joueur=0
+    score_du_croupier=0
+
+
+    carte=FctBlackJack.carte_a_distribuer(jeu)
+    print(carte[0])
+    main=FctBlackJack.main_joueur(carte[0],joueur,main_du_joueur,main_du_croupier,score_du_joueur,score_du_croupier)
+
+    carte2=FctBlackJack.carte_a_distribuer(carte[1])
+    print(carte2[0])
+    main2=FctBlackJack.main_joueur(carte2[0],main[2],main[3],main[0],main[4],main[1])
+
+    carte3=FctBlackJack.carte_a_distribuer(carte[1])
+    print(carte3[0])
+    main3=FctBlackJack.main_joueur(carte3[0],main2[2],main2[3],main2[0],main2[4],main2[1])
+
+    carte4=FctBlackJack.carte_a_distribuer(carte[1])
+    print(carte4[0])
+    main4=FctBlackJack.main_joueur(carte4[0],main3[2],main3[3],main3[0],main3[4],main3[1])
+
+    print(f"main du joueur : {main4[3]} ")#joueur 
+    print(f"main du croupier : {main4[0]}" )#croupier
+
+    mainJ_label.config(text=f"main joueur : {main4[3]}")
 
     root_jeu.mainloop()
