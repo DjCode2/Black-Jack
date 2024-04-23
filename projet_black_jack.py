508cca48df3873ab1eb37d8d9aaff595dd08aba3
DjCode2 <99595748+DjCode2@users.noreply.github.com>
2024-04-23 15:50:00 +0200

    Update projet_black_jack.py

--- projet_black_jack.py
+++ projet_black_jack.py
@@ -1,5 +1,6 @@
 # projet black jack
 """Clément, Théo, Maïwenn"""
+
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
@@ -7,13 +8,18 @@ from PIL import Image, ImageTk
 import random
 import tkinter as tk
 import numpy as np
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk   
+
 
 
 def ajouter_au_score(valeur, label):
     label.config(text=f"Score : {valeur}")
 
-
+def somme_valeurs(cartes):
+    total = 0
+    for carte in cartes:
+        total += carte[2]  # La troisième valeur dans chaque tuple est la valeur numérique
+    return total
 
 
 def redimensionner_image(image, taille):
@@ -33,9 +39,16 @@ def redimensionner_image(image, taille):
     return img_redimensionnee
 
 
+
+
+def nettoyer_cartes(liste_cartes):
+    liste_nettoyee = []
+    for carte in liste_cartes:
+        if not isinstance(carte, list):
+            liste_nettoyee.append(carte)
+    return liste_nettoyee
+
 # generation du paquet de carte 
-
-
 def jeu_de_carte ():
 
     global jeu
@@ -55,17 +68,19 @@ def jeu_de_carte ():
 
 def changement_joueur():
 
-    global joueur
-
-    print(joueur)
-    #nombre_de_joueur=int(input("combien de joueur?"))
-    #assert nombre_de_joueur<5 and nombre_de_joueur>0
     # numpy array et implementer avec le nombre de joueur donner 
     #ou for de nombre de joueur et crer une list
-    if joueur==1: #joueur
-        joueur=2 #croupier
+
+    global nb_joueur
+    global joueur
+    
+    if joueur=="croupier":
+        joueur=0
+    elif joueur<nb_joueur-1:
+        joueur+=1
     else :
-        joueur=1
+        joueur="croupier"
+
     return 
 
 # fonction determiner la carte distribué + actualisation du jeu de carte 
@@ -74,8 +89,8 @@ def carte_a_distribuer():
     global jeu
     global carte
     
-    nombre=random.randint(0,len(jeu))
-    carte=jeu[nombre] #bug ici
+    nombre=random.randint(0,len(jeu)-1)
+    carte=jeu[nombre]
 
     if carte[1]=="as":
         valeur_as=int(input("1 ou 11"))
@@ -91,51 +106,83 @@ def main_joueur():
 
     global carte
     global joueur
-    global main_du_joueur
+    global liste_main_du_joueur
+    global liste_score_du_joueurscore_du_joueur
     global main_du_croupier
-    global score_du_joueur
     global score_du_croupier
+    x = 673
+    
 
-    if joueur==1:
-        main_du_joueur.append(carte)
-        score_du_joueur+=carte[2]
-    else:
+    
+    if joueur=="croupier":
         main_du_croupier.append(carte)
         score_du_croupier+=carte[2]
+        #print (main_du_croupier)
+        #print (score_du_croupier)
+    else:
+        main_du_joueur=liste_main_du_joueur[joueur]
+        score_du_joueur=liste_score_du_joueur[joueur]
+        main_du_joueur.append(carte)
+        main_du_joueur_netoyer = nettoyer_cartes(main_du_joueur)
+
+        #print(main_du_joueur_netoyer)
+        score_du_joueur[0]+=carte[2]
+        #print (main_du_joueur)
+        #print (score_du_joueur)
+        liste_main_du_joueur[joueur].append(main_du_joueur)
+        liste_score_du_joueur[joueur]=score_du_joueur
     return
 
-def somme_valeurs(cartes):
-    total = 0
-    for carte in cartes:
-        total += carte[2]  # La troisième valeur dans chaque tuple est la valeur numérique
-    return total
-
-def action_joueur(mainJ_label,score_label,canvas):
+def action_joueur():
 
     global jeu
     global carte
     global joueur
-    global main_du_joueur
-    global score_du_joueur
+    global liste_main_du_joueur
+    global liste_score_du_joueur
+    score_du_joueur=liste_score_du_joueur[joueur]
+    main_du_joueur=liste_main_du_joueur[joueur]
 
-    if score_du_joueur<21:
-        carte_a_distribuer()
-        main_joueur()
+    if score_du_joueur[0]<21:
+        carte_sup=input("saisissez 'carte' si vous souahiter une carte en plus")
+        #assert carte_sup=="carte"
+        if carte_sup=="carte":
+            carte_supplementaire=int(input("saisissez le nombre de carte en plus"))
+            for i in range (carte_supplementaire):
+                carte_a_distribuer()
+                main_joueur()
+        else :
+            print("error")
+    
+    return   
 
-        print(main_du_joueur)
-        mainJ_label.config(text=f"main joueur : {main_du_joueur}")
-        ajouter_au_score(somme_valeurs(main_du_joueur),score_label)
+def carte_en_plus(mainJ_label,canvas):
 
-        img = "cartes/" + str(main_du_joueur[2]) + ".gif"  # Chemin d'accès à l'image
+    global jeu
+    global carte
+    global joueur
+    global liste_main_du_joueur
+    global liste_score_du_joueur
+    score_du_joueur=liste_score_du_joueur[joueur]
+    main_du_joueur=liste_main_du_joueur[joueur]
+
+    if score_du_joueur[0]<21:
+
+        #print(main_du_joueur)
+        mainJ_label.config(text=f"main joueur : {liste_main_du_joueur[0]}")
+
+        img = "cartes/" + str(nettoyer_cartes(liste_main_du_joueur[0])[-1]) + ".gif"  # Chemin d'accès à l'image
         img_redim = redimensionner_image(img, (80, 120))
         canvas.create_image(730, 495, anchor="nw", image=img_redim)
+        print("pipou carte afi")
 
         action_croupier()
         fin_de_jeu()
+
     else :
-        print(2)
+        fin_de_jeu()
     
-    return main_du_joueur
+    return          
 
 def action_croupier():
 
@@ -152,33 +199,49 @@ def action_croupier():
 
 def fin_de_jeu():
 
-    global main_du_joueur
-    global score_du_joueur
+    global liste_main_du_joueur
+    global liste_score_du_joueur
+    main_du_joueur=liste_main_du_joueur[joueur]
+    score_du_joueur=liste_score_du_joueur[joueur]
     global score_du_croupier
 
-    if score_du_joueur>21 or score_du_joueur<score_du_croupier and score_du_croupier<21 :
+    if score_du_joueur[0]>21 or score_du_joueur[0]<score_du_croupier and score_du_croupier<=21 :
         print ("perdu")
-    elif score_du_joueur==21 and len(main_du_joueur)==2:
+    elif score_du_joueur[0]==21 and len(main_du_joueur)==2:
         print ("black jack")
-    elif score_du_joueur==score_du_croupier or score_du_joueur==21 and score_du_joueur==score_du_croupier :
+    elif score_du_joueur[0]==score_du_croupier or score_du_joueur[0]==21 and score_du_joueur[0]==score_du_croupier :
         print ("egalité")
     else :
         print ("gagner")
 
+def generation_main_joueur():
+    global nb_joueur
+    liste_main=[]
+    for i in range (nb_joueur):
+        liste_main.append([])
+    print (liste_main)
+
+    return liste_main
+
+def generation_score_joueur():
+    global nb_joueur
+    liste_scrore=[]
+    for i in range (nb_joueur):
+        liste_scrore.append([0])
+
+    return liste_scrore
+
 # programme principale 
 jeu=[]
 carte=0 
-joueur=1
+joueur=0
+nb_joueur=2
 
+liste_main_du_joueur=generation_main_joueur()
 main_du_joueur=[] 
 main_du_croupier=[]
 
+liste_score_du_joueur=generation_score_joueur()
 score_du_joueur=0
 score_du_croupier=0
-
-
-
-
-
-#fonction double mise faite 
 
