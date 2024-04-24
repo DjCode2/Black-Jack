ffa2ce9f9815d0caff036b42c43d137f00532a98
DjCode2 <99595748+DjCode2@users.noreply.github.com>
2024-04-24 21:20:18 +0200

    Update param.py

--- param.py
+++ param.py
@@ -1,49 +1,43 @@
 import tkinter as tk
 import fonction
 
-def obtenir_valeurs(curseur_mise,curseur_joueurs,root_param):
-    nb_joueurs = int(curseur_joueurs.get())
-    mise = curseur_mise.get()
+# Définir la fonction obtenir_valeurs
+def obtenir_valeurs(entree_nb_joueurs, entree_mise):
+    global nb_joueurs  
+    global mise 
+    nb_joueurs = int(entree_nb_joueurs.get())
+    mise = int(entree_mise.get())
+    return nb_joueurs, mise
 
-    nb_joueur_lst=[]
-    for i in range(1, nb_joueurs+1):
-        nb_joueur_lst.append(i) #liste allant de 1 au nombre de joueur, pour après pouvoir faire
-        #5 systèmes de cartes et les lancer que si leur numéro est dans la liste
-    
-    print("Mise sélectionnée (en euros) :", mise)
-    print("liste de joueur", nb_joueur_lst)
-
-    fonction.BouclePrincipale(root_param)
-
-
-
-
-
+# Définir la fonction paramètre
 def paramètre(root_menu):
+    global nb_joueurs 
+    global mise
     root_menu.destroy()
     root_param = tk.Tk()
     root_param.title("Choix des joueurs et de la mise")
 
-    # Cadre principal + titre 
     fenetre = tk.Frame(root_param)
-    label_param = tk.Label(root_param, text="Paramètre : ",font=("Arial", 16))
-    label_param.pack(padx=40, pady=20)
     fenetre.pack(padx=40, pady=10)
 
-    # Curseur Snombre de joueurs
-    label_joueur = tk.Label(fenetre, text="Nombre de joueurs souhaité:")
-    label_joueur.grid(row=0, column=0, padx=10, pady=5)
-    curseur_joueurs = tk.Scale(fenetre, from_=1, to=5, orient=tk.HORIZONTAL, length=200)
-    curseur_joueurs.grid(row=0, column=1, padx=10, pady=5)
+    # Entrée pour choisir le nombre de joueurs
+    label_nb_joueurs = tk.Label(fenetre, text="Nombre de joueurs souhaité (entre 2 et 3):")
+    label_nb_joueurs.grid(row=0, column=0, padx=10, pady=5)
+    entree_nb_joueurs = tk.Entry(fenetre)
+    entree_nb_joueurs.grid(row=0, column=1, padx=10, pady=5)
 
-    # Curseur mise
+    # Entrée pour choisir la mise
     label_mise = tk.Label(fenetre, text="Mise (en euros) :")
     label_mise.grid(row=1, column=0, padx=10, pady=5)
-    curseur_mise = tk.Scale(fenetre, from_=1, to=100, orient=tk.HORIZONTAL, length=200)
-    curseur_mise.grid(row=1, column=1, padx=10, pady=5)
+    entree_mise = tk.Entry(fenetre)
+    entree_mise.grid(row=1, column=1, padx=10, pady=5)
 
     # Bouton valider
-    bouton_valider = tk.Button(fenetre, text="Valider", command=lambda : fonction.BouclePrincipale(root_param))
+    bouton_valider = tk.Button(fenetre, text="Valider", command=lambda : obtenir_valeurs(entree_nb_joueurs, entree_mise))
     bouton_valider.grid(row=2, columnspan=2, padx=10, pady=10)
+
+    # Bouton jouer
+    bouton_jouer = tk.Button(fenetre, text="Jouer", command=lambda : fonction.BouclePrincipale(root_param))
+    bouton_jouer.grid(row=3, columnspan=2, padx=10, pady=10)
 
     root_param.mainloop()
